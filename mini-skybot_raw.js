72aa71fc998b7f40c9f809640d2c096a445f1036
Mark Moissette <kaosat.dev@gmail.com>
2014-01-18 11:33:34 +0100

    -fleshed out the raw js example skybot a bit more

--- mini-skybot_raw.js
+++ mini-skybot_raw.js
@@ -3,16 +3,33 @@ function BigWheel(options)
   Part.call( this );
   this.radius 		= options.r || 20 ;
   this.servoHorn  = options.mount || new CircleServoHorn();
+
+  this.union( new Cylinder({r:this.radius}) );
+  this.subtract( this.servoHorn );
   
 }
 BigWheel.prototype = Object.create( Part.prototype );
 BigWheel.prototype.constructor = BigWheel;
 
 
+function CircleServoHorn(options)//technically, non printable !
+{
+  this.outerRadius = options.or || 10;
+  this.servoRadius = options.sr || 3;
+
+  this.union( new Cylinder({r:this.outerRadius}) );
+  this.subtract( new Cylinder({r:this.servoRadius}) );
+}
+CircleServoHorn.prototype = Object.create( Part.prototype );
+CircleServoHorn.prototype.constructor = CircleServoHorn;
+
+
 function SmallWheel(options)
 {
   Part.call( this );
  	this.radius = options.r || 5;
+
+  this.union( new Sphere({r:this.radius}) );
 }
 SmallWheel.prototype = Object.create( Part.prototype );
 SmallWheel.prototype.constructor = SmallWheel;
@@ -21,11 +38,40 @@ function Chassis(options)
 {
   Part.call( this );
   this.size = options.size || [30,50,30];
+
+  this.union( new Cube({size:this.size}) )
 }
 Chassis.prototype = Object.create( Part.prototype );
 Chassis.prototype.constructor = Chassis;
 
+/*Main "wrapper"/container*/
+function Skybot(options)
+{
+  Part.call( this );
+  this.bodySize       = options.bodySize || [30,50,30];
+  this.bigWheelRadius = options.bigWheelR || 30;
 
+  this.leftWheel  = new BigWheel();
+  this.rightWheel = new BigWheel();
+  this.backWheel  = new SmallWheel();
+  this.body       = new Chassis();
+
+  this.add(this.body);
+  this.add( this.leftWheel.translate([this.bodySize.x/2,0,0]) );
+  this.add( this.rightWheel.translate([-this.bodySize.x/2,0,0]) );
+  this.add(this.backWheel.translate([0,-this.bodySize.y,0]));
+}
+Skybot.prototype = Object.create( Part.prototype );
+Skybot.prototype.constructor = Skybot;
+
+/*do the assembly*/
+
+var skybot = new Skybot();
+
+assembly.add(skybot);
+
+
+////////////////////////////////////
 function BatteryHolder(options)
 {
 }
