aa1341511ff9f8bcdb7a0337058c68749e29912c
Mark Moissette <kaosat.dev@gmail.com>
2014-03-04 13:31:15 +0100

    - updated shapes2d to have a better system for updating control points based on their visual counterparts:  * modified moveTo, lineTo , quadraticCurveTo, to reuse 2d vectors: the same instances of vector2d are now used in the curves, the actions and in the custom "controlPoints" instance attribute  * added override for getPoints, modified in accordance to the changes above  * added event handling for VISUAL controls so that the real control points can change based on the transformations of the visual controls  * added a fromVector2 method to THREE.Vector3

--- custom-elements/visual-editor/lib/shapes/shapes2d.js
+++ custom-elements/visual-editor/lib/shapes/shapes2d.js
@@ -1,3 +1,10 @@
+//HELPER, not so sure about this:
+
+THREE.Vector3.fromVector2 = function( vec2 )
+{
+    return new THREE.Vector3(vec2.x, vec2.y, 0);
+}
+
 function Shape2d()
 {
   THREE.Shape.apply( this, arguments );
@@ -7,20 +14,44 @@ function Shape2d()
 Shape2d.prototype = Object.create( THREE.Shape.prototype );
 Shape2d.__id = 0;
 
+//this needs to inject new lineTo, bezierCurveTo elements
+Shape2d.prototype.addPoint = function(x, y)
+{
+  throw new Error("Not implemented yet");
+  //this.getPoint(x,y);
+}
+
 Shape2d.prototype.moveTo= function(x,y)
 {
   console.log("moveTo");
-  this.controlPoints.push( new THREE.Vector3(x,y,0) );
-  THREE.Shape.prototype.moveTo.call(this, x,y);
+  //this.controlPoints.push( new THREE.Vector3(x,y,0) );
+  var args = Array.prototype.slice.call( arguments );
+  var controlPoint = new THREE.Vector2( x, y );
+  controlPoint.index = this.actions.length;
+  this.controlPoints.push( controlPoint );
+
+  
+	this.actions.push( { action: THREE.PathActions.MOVE_TO, args: args, args2:controlPoint } );
+  
+  //THREE.Shape.prototype.moveTo.call(this, x,y);
 } 
 
 Shape2d.prototype.lineTo= function(x,y)
 {
   console.log("lineTo");
-  this.controlPoints.push( new THREE.Vector3(x,y,0) );
-  THREE.Shape.prototype.lineTo.call(this, x,y);
-} 
+  //this.controlPoints.push( new THREE.Vector3(x,y,0) );
+  var args = Array.prototype.slice.call( arguments );
+  var endPoint = new THREE.Vector2( x, y );
+  endPoint.index = this.actions.length;
+  this.controlPoints.push( endPoint );
 
+	var lastArgs = this.actions[ this.actions.length - 1 ].args2;
+	var prevPoint= lastArgs;
+
+	var curve = new THREE.LineCurve( prevPoint, endPoint );
+	this.curves.push( curve );
+	this.actions.push( { action: THREE.PathActions.LINE_TO, args: args, args2:endPoint } );
+} 
 
 Shape2d.prototype.bezierCurveTo= function(aCP1x, aCP1y, aCP2x, aCP2y, aX, aY)
 {
@@ -28,6 +59,28 @@ Shape2d.prototype.bezierCurveTo= function(aCP1x, aCP1y, aCP2x, aCP2y, aX, aY)
   this.controlPoints.push( new THREE.Vector3(aX,aY,0) );
   THREE.Shape.prototype.bezierCurveTo.call(this, aCP1x, aCP1y, aCP2x, aCP2y, aX, aY);
 } 
+
+Shape2d.prototype.quadraticCurveTo = function( aCPx, aCPy, aX, aY )
+{
+  console.log("quadraticCurveTo");
+  var args = Array.prototype.slice.call( arguments );
+  var endPoint = new THREE.Vector2( aX, aY );
+  endPoint.index = this.actions.length;
+  this.controlPoints.push( endPoint );
+
+  var lastArgs = this.actions[ this.actions.length - 1 ].args2;
+	var prevPoint= lastArgs;
+	
+	var curveControlPoint = new THREE.Vector2( aCPx, aCPy );
+  curveControlPoint.index = this.actions.length;
+
+	var curve = new THREE.QuadraticBezierCurve( prevPoint,
+												curveControlPoint,
+												endPoint );
+	this.curves.push( curve );
+
+	this.actions.push( { action: THREE.PathActions.QUADRATIC_CURVE_TO, args: args, args2:endPoint,args3:curveControlPoint } );
+}
 
 Shape2d.prototype.fromExpression = function( expression )
 {
@@ -43,8 +96,6 @@ Shape2d.prototype.update = function()
 {
   var controlPoints = this.superDuperControls;
   var actions = this.actions;
-  
-  //console.log("curves", this.curves);
   
   for(var i=0;i<controlPoints.length;i++)
   {
@@ -62,7 +113,6 @@ Shape2d.prototype.update = function()
     
     this.curves[ controlPoints[i].curveIndex ] = new THREE.LineCurve(controlPoints[i].originalCurve.v1,controlPoints[i].original);*/
   }
-  
   this.cacheLengths = null;
   this.needsUpdate = true;
   
@@ -92,13 +142,16 @@ Shape2d.prototype.generateRenderables = function()
 			  var color = color || 0x3333FF;
         var pointHelper = new THREE.Mesh(new THREE.CubeGeometry(controlSize,controlSize,controlSize), new THREE.MeshBasicMaterial({color:color}));
         pointHelper.name = "Shape2dPointHelper";
-        var position = new THREE.Vector3(pt.x, pt.y,0)
+        var position = THREE.Vector3.fromVector2( pt ); //new THREE.Vector3(pt.x, pt.y,0)
 			  pointHelper.position= position;
 			  line.add( pointHelper );
 			  
-			  //for testing
-			  this.addEventListener('controlPointChanged', function(event) {alert("GOT THE EVENT");});
-        pointHelper.dispatchEvent({type:'controlPointChanged'});
+			  //for testing: update the 2d point the helper stands in for based on the helper's position
+			  pointHelper.addEventListener('translated', function(event) {
+			    //console.log("control point translated",event,this);
+			    this.standInFor.x = this.position.x;
+			    this.standInFor.y = this.position.y;
+			  });
 			  
 			  return pointHelper;
 			}
@@ -108,32 +161,48 @@ Shape2d.prototype.generateRenderables = function()
 
 		    action = item.action;
 		    args = item.args;
+		    args2 = item.args2;
 
 		    switch( action ) {
 
 		    case THREE.PathActions.MOVE_TO:
-          var pt = new THREE.Vector2( args[ 0 ], args[ 1 ] )
+		      console.log("case MoveTo, args", args2);
+          var pt = args2;//new THREE.Vector2( args[ 0 ], args[ 1 ] )
           var helper = drawPointHelper(pt);
+          helper.standInFor = pt;
           var pos = helper.position;
           pos.actionIndex =i;
           this.superDuperControls.push( pos );
 			    break;
 			    
 		    case THREE.PathActions.LINE_TO:
-          var pt = new THREE.Vector2( args[ 0 ], args[ 1 ] )
+		      console.log("case LINETo, args", args2);
+          var pt = args2;//new THREE.Vector2( args[ 0 ], args[ 1 ] )
           var helper = drawPointHelper(pt);
+          helper.standInFor = pt;
           var pos = helper.position;
           pos.actionIndex =i;
           this.superDuperControls.push( pos );
 			    break;
 			   
 			  case THREE.PathActions.QUADRATIC_CURVE_TO:
-			    var pt = new THREE.Vector2( args[ 2 ], args[ 3 ] )
+			    var pt = args2;//new THREE.Vector2( args[ 2 ], args[ 3 ] )
           var helper = drawPointHelper(pt);
+          helper.standInFor = pt;
           var pos = helper.position;
           pos.actionIndex =i;
           pos.argIndices = [2,3];
           this.superDuperControls.push( pos );
+          
+          var args3 = item.args3;
+          pt = args3;//new THREE.Vector2( args[ 0 ], args[ 1] );
+			    var curveHelper =drawPointHelper(pt, 0xff00ff);
+			    curveHelper.standInFor = pt;
+			    pos = curveHelper.position;
+			    pos.actionIndex =i;
+			    pos.argIndices = [0,1];
+			    this.superDuperControls.push( pos );
+          
           break;
 			  
 			  case THREE.PathActions.BEZIER_CURVE_TO:
@@ -264,6 +333,242 @@ Shape2d.prototype.generateRenderables = function()
 	  this.renderable = line;
 	  return this.renderable;
 }
+
+/* Return an array of vectors based on contour of the path */
+
+Shape2d.prototype.getPoints = function( divisions, closedPath ) {
+	if (this.useSpacedPoints) {
+		console.log('tata');
+		return this.getSpacedPoints( divisions, closedPath );
+	}
+
+	divisions = divisions || 12;
+
+	var points = [];
+
+	var i, il, item, action, args;
+	var cpx, cpy, cpx2, cpy2, cpx1, cpy1, cpx0, cpy0,
+		laste, j,
+		t, tx, ty;
+
+	for ( i = 0, il = this.actions.length; i < il; i ++ ) {
+
+		item = this.actions[ i ];
+
+		action = item.action;
+		args = item.args;
+		args2 = item.args2;
+		args3 = item.args3;
+
+		switch( action ) {
+
+		case THREE.PathActions.MOVE_TO:
+
+			points.push( args2 );
+
+			break;
+
+		case THREE.PathActions.LINE_TO:
+
+			points.push( args2 );
+
+			break;
+
+		case THREE.PathActions.QUADRATIC_CURVE_TO:
+
+			cpx  = args2.x;
+			cpy  = args2.y;
+
+      cpx1 = args3.x;
+      cpy1 = args3.y;
+
+			if ( points.length > 0 ) {
+
+				last = points[ points.length - 1 ];
+				cpx0 = last.x;
+				cpy0 = last.y;
+
+			} else {
+        console.log("gne");
+				last = this.actions[ i - 1 ].args2;
+
+        cpx0 = last.x;
+        cpy0 = last.y;
+				//cpx0 = laste[ laste.length - 2 ];
+				//cpy0 = laste[ laste.length - 1 ];
+
+			}
+
+			for ( j = 1; j <= divisions; j ++ ) {
+
+				t = j / divisions;
+
+				tx = THREE.Shape.Utils.b2( t, cpx0, cpx1, cpx );
+				ty = THREE.Shape.Utils.b2( t, cpy0, cpy1, cpy );
+
+				points.push( new THREE.Vector2( tx, ty ) );
+
+			}
+
+			break;
+
+		case THREE.PathActions.BEZIER_CURVE_TO:
+
+			cpx  = args[ 4 ];
+			cpy  = args[ 5 ];
+
+			cpx1 = args[ 0 ];
+			cpy1 = args[ 1 ];
+
+			cpx2 = args[ 2 ];
+			cpy2 = args[ 3 ];
+
+			if ( points.length > 0 ) {
+
+				laste = points[ points.length - 1 ];
+
+				cpx0 = laste.x;
+				cpy0 = laste.y;
+
+			} else {
+
+				laste = this.actions[ i - 1 ].args;
+
+				cpx0 = laste[ laste.length - 2 ];
+				cpy0 = laste[ laste.length - 1 ];
+
+			}
+
+
+			for ( j = 1; j <= divisions; j ++ ) {
+
+				t = j / divisions;
+
+				tx = THREE.Shape.Utils.b3( t, cpx0, cpx1, cpx2, cpx );
+				ty = THREE.Shape.Utils.b3( t, cpy0, cpy1, cpy2, cpy );
+
+				points.push( new THREE.Vector2( tx, ty ) );
+
+			}
+
+			break;
+
+		case THREE.PathActions.CSPLINE_THRU:
+
+			laste = this.actions[ i - 1 ].args;
+
+			var last = new THREE.Vector2( laste[ laste.length - 2 ], laste[ laste.length - 1 ] );
+			var spts = [ last ];
+
+			var n = divisions * args[ 0 ].length;
+
+			spts = spts.concat( args[ 0 ] );
+
+			var spline = new THREE.SplineCurve( spts );
+
+			for ( j = 1; j <= n; j ++ ) {
+
+				points.push( spline.getPointAt( j / n ) ) ;
+
+			}
+
+			break;
+
+		case THREE.PathActions.ARC:
+
+			var aX = args[ 0 ], aY = args[ 1 ],
+				aRadius = args[ 2 ],
+				aStartAngle = args[ 3 ], aEndAngle = args[ 4 ],
+				aClockwise = !!args[ 5 ];
+
+			var deltaAngle = aEndAngle - aStartAngle;
+			var angle;
+			var tdivisions = divisions * 2;
+
+			for ( j = 1; j <= tdivisions; j ++ ) {
+
+				t = j / tdivisions;
+
+				if ( ! aClockwise ) {
+
+					t = 1 - t;
+
+				}
+
+				angle = aStartAngle + t * deltaAngle;
+
+				tx = aX + aRadius * Math.cos( angle );
+				ty = aY + aRadius * Math.sin( angle );
+
+				//console.log('t', t, 'angle', angle, 'tx', tx, 'ty', ty);
+
+				points.push( new THREE.Vector2( tx, ty ) );
+
+			}
+
+			//console.log(points);
+
+		  break;
+
+		case THREE.PathActions.ELLIPSE:
+
+			var aX = args[ 0 ], aY = args[ 1 ],
+				xRadius = args[ 2 ],
+				yRadius = args[ 3 ],
+				aStartAngle = args[ 4 ], aEndAngle = args[ 5 ],
+				aClockwise = !!args[ 6 ];
+
+
+			var deltaAngle = aEndAngle - aStartAngle;
+			var angle;
+			var tdivisions = divisions * 2;
+
+			for ( j = 1; j <= tdivisions; j ++ ) {
+
+				t = j / tdivisions;
+
+				if ( ! aClockwise ) {
+
+					t = 1 - t;
+
+				}
+
+				angle = aStartAngle + t * deltaAngle;
+
+				tx = aX + xRadius * Math.cos( angle );
+				ty = aY + yRadius * Math.sin( angle );
+
+				//console.log('t', t, 'angle', angle, 'tx', tx, 'ty', ty);
+
+				points.push( new THREE.Vector2( tx, ty ) );
+
+			}
+
+			//console.log(points);
+
+		  break;
+
+		} // end switch
+
+	}
+
+
+
+	// Normalize to remove the closing point by default.
+	var lastPoint = points[ points.length - 1];
+	var EPSILON = 0.0000000001;
+	if ( Math.abs(lastPoint.x - points[ 0 ].x) < EPSILON &&
+			 Math.abs(lastPoint.y - points[ 0 ].y) < EPSILON)
+		points.splice( points.length - 1, 1);
+	if ( closedPath ) {
+
+		points.push( points[ 0 ] );
+
+	}
+
+	return points;
+
+};
 
 
 
