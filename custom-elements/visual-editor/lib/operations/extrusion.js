91d80bf6ba63ab73706368ad85a284bc324ccdc4
Mark Moissette <kaosat.dev@gmail.com>
2014-03-04 10:09:30 +0100

    - fleshed out extrusion operation with all relevant fields:  * value now holds the parameters used for extruding  * sourceShape holds the instance of the shape the extrusion was made from

--- custom-elements/visual-editor/lib/operations/extrusion.js
+++ custom-elements/visual-editor/lib/operations/extrusion.js
@@ -1,15 +1,17 @@
-Extrusion = function (target, parentObject)
+Extrusion = function (target, value, sourceShape, parentObject)
 {
   Command.call( this );
   this.type = "extrusion";
   this.target = target;
+  this.value = value;
+  this.sourceShape = sourceShape;
   this.parentObject = parentObject;
 }
 Extrusion.prototype = Object.create( Command.prototype );
 Extrusion.prototype.constructor=Extrusion;
 Extrusion.prototype.clone = function()
 {
-  return new Extrusion( this.target, this.parentObject);
+  return new Extrusion( this.target, this.value, this.sourceShape, this.parentObject);
 }
 
 Extrusion.prototype.undo = function()
