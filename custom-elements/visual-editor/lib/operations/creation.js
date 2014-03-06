68e3179d9bc6e6e9a5866dbeaeec7596c1e7bef5
Mark Moissette <kaosat.dev@gmail.com>
2014-03-06 13:44:27 +0100

    - added "value" field to "Creation" operation:  * it gets filled with the parameters used to create an object

--- custom-elements/visual-editor/lib/operations/creation.js
+++ custom-elements/visual-editor/lib/operations/creation.js
@@ -1,15 +1,16 @@
-Creation = function (target, parentObject)
+Creation = function (target, parentObject, options)
 {
   Command.call( this );
   this.type = "creation";
   this.target = target;
   this.parentObject = parentObject;
+  this.value = options;
 }
 Creation.prototype = Object.create( Command.prototype );
 Creation.prototype.constructor=Creation;
 Creation.prototype.clone = function()
 {
-  return new Creation( this.target, this.parentObject);
+  return new Creation( this.target, this.parentObject, this.value);
 }
 
 Creation.prototype.undo = function()
