9b7b2f6638e1d570ea74df6f17b5654bd3ea8a1c
Mark Moissette <kaosat.dev@gmail.com>
2014-03-15 17:15:51 +0100

    - added basis for "mirror" operation (not functional yet)

---
+++ custom-elements/visual-editor/lib/operations/mirror.js
@@ -0,0 +1,23 @@
+Mirror = function ( target, axis)
+{
+  Command.call( this );
+  this.type = "mirroring";
+  this.target = target;
+  this.value = axis
+}
+Mirror.prototype = Object.create( Command.prototype );
+Mirror.prototype.constructor=Translation;
+Mirror.prototype.clone = function()
+{
+  return new Mirror( this.target, this.value);
+}
+
+Mirror.prototype.undo = function()
+{
+    this.target.mirror(this.value);
+}
+
+Mirror.prototype.redo = function()
+{
+    this.target.mirror(this.value);
+}
