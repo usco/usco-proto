dffe33637dfdbf384e8cf2d5120bd4cd0d25b7f7
Mark Moissette <kaosat.dev@gmail.com>
2014-03-21 14:39:22 +0100

    - added basis of dynamic class creator (or "templatizer"):  * very basic for now but it works  * allows creating a new custom class from a list of selected shapes/parts  * for now, position/rotation/scale etc or history of selections is not transfered to new class

---
+++ custom-elements/visual-editor/lib/class-creator/class-creator.js
@@ -0,0 +1,50 @@
+
+
+function createCustomPartClass( className, selections )
+{
+  var className = className || "CustomPart";
+  var selections = selections || [];
+  
+  var transformedSelections = [];
+  for(var i=0;i<selections.length;i++)
+  {
+    var selection = selections[i].sourceShape;
+    var type = selection.constructor.name;//.toLowerCase()
+    transformedSelections.push("var foo"+i+" = new "+type+"();");
+    transformedSelections.push("foo"+i+".position.z =30;");
+    transformedSelections.push("this.add(foo"+i+");");
+  }
+  
+  var rawClassText=[
+  "function "+className+"(options)",
+  "{" ,
+  " options = options || {};",
+  "  Part.call( this );",
+  "  this.name = '"+className+"'+this.id;",
+  //"  this.geometry = new THREE.CubeGeometry( 15, 5, 10 );",
+  "  this.blabla = 24;",
+  "}",
+  className+".prototype = Object.create( Part.prototype );",
+  className+".prototype.constructor = "+className+";",
+  
+  //not sure about "execute", but this is inspired by freecad python API
+  //TODO: this is where all our current selections get added 
+  className+".prototype.execute = function()",
+  "{",
+   transformedSelections.join("\n"), 
+  "}",
+  "return "+className+";"
+  ]
+  
+
+  rawClassText = rawClassText.join("\n");
+  console.log("rawClassText:\n\n",rawClassText);
+  
+  args = "";
+  //var newClass = eval( rawClassText );
+  var newClass = new Function(rawClassText)();
+  //console.log("newClass", newClass);
+  console.log("custom class:\n", newClass);
+  return newClass;
+
+}
