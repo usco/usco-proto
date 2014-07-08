9a763b97c69af557f7bd6ecfaf960a90e7e8481d
Mark Moissette <kaosat.dev@gmail.com>
2014-07-08 23:25:26 +0200

    - added start end and direction params - lots of minor cosmetic improvements

--- custom-elements/visual-helpers/DistanceHelper.js
+++ custom-elements/visual-helpers/DistanceHelper.js
@@ -1,6 +1,16 @@
 DistanceHelper = function(options)
 {
   var options = options || {};
+  var start = options.start || new THREE.Vector3();
+  var end = options.end || new THREE.Vector3(10,0,0);
+  var direction = new THREE.Vector3(1,0,0);
+  
+  if(end && start)
+  {
+    options.length = end.clone().sub( start ).length();
+    var direction = (end.clone().sub(start)).normalize();
+  }
+  
   var length = this.length = options.length || 50;
 
   var crossSize = 10;
@@ -18,17 +28,23 @@ DistanceHelper = function(options)
   startCrossGeometry.vertices.push( new THREE.Vector3( -crossSize/2, 0, 0 ) );
   startCrossGeometry.vertices.push( new THREE.Vector3( crossSize/2, 0 , 0 ) );
   
-  this.startCross = new THREE.Line( startCrossGeometry, new THREE.LineBasicMaterial( { color: 0x000000,depthTest:false,depthWrite:false,renderDepth : 1e20, opacity:0.4, transparent:true } ),THREE.LinePieces );
+  var crossMaterial = new THREE.LineBasicMaterial( { color: 0x000000,depthTest:false,depthWrite:false,renderDepth : 1e20, opacity:0.4, transparent:true } )
+  var lineMaterial = new THREE.LineDashedMaterial( { color: 0xff0000, dashSize: 1.5, gapSize: 25,linewidth:1} )
+  this.startCross = new THREE.Line( startCrossGeometry, crossMaterial ,THREE.LinePieces );
   this.add( this.startCross ) ;
   
   //main arrow
-  this.arrow = new THREE.ArrowHelper(new THREE.Vector3(1,0,0), new THREE.Vector3(0,0,0 ) , length ,0x000000, 4, 2);
+  this.arrow = new THREE.ArrowHelper(direction, start, length ,0xff0000, 14, 5);
   this.arrow.scale.z =0.1;
+  this.arrow.line.geometry.computeLineDistances();
+  this.arrow.line.material = lineMaterial;
   this.add( this.arrow ) ;
   
   //length label
   this.label = new LabelHelper3d({text:this.text,fontSize:this.textSize});
-  this.label.position.x = length/2;
+  //at midpoint
+  var labelPos = start.clone().add( direction.clone().multiplyScalar( length/2 ) );
+  this.label.position = labelPos;  //.x = length/2;
   
   this.add( this.label );
 }
